906bf4904c2b6f2ad45433fb3d50fe2dc53dfb87
Plaiasu Andrei Nicolae <plaiasuandrei@yahoo.com>
2016-11-11 20:52:05 +0000

    working version

--- webapp/controller/Detail.controller.js
+++ webapp/controller/Detail.controller.js
@@ -1,179 +1,199 @@
 /*global location */
 sap.ui.define([
-		"rtl/controller/BaseController",
-		"sap/ui/model/json/JSONModel",
-		"rtl/model/formatter"
-	], function (BaseController, JSONModel, formatter) {
-		"use strict";
+	"rtl/controller/BaseController",
+	"sap/ui/model/json/JSONModel",
+	"rtl/model/formatter"
+], function(BaseController, JSONModel, formatter) {
+	"use strict";
 
-		return BaseController.extend("rtl.controller.Detail", {
+	return BaseController.extend("rtl.controller.Detail", {
 
-			formatter: formatter,
+		formatter: formatter,
 
-			/* =========================================================== */
-			/* lifecycle methods                                           */
-			/* =========================================================== */
+		/* =========================================================== */
+		/* lifecycle methods                                           */
+		/* =========================================================== */
 
-			onInit : function () {
-				// Model used to manipulate control states. The chosen values make sure,
-				// detail page is busy indication immediately so there is no break in
-				// between the busy indication for loading the view's meta data
-				var oViewModel = new JSONModel({
-					busy : false,
-					delay : 0,
-					lineItemListTitle : this.getResourceBundle().getText("detailLineItemTableHeading")
-				});
+		onInit: function() {
+			// Model used to manipulate control states. The chosen values make sure,
+			// detail page is busy indication immediately so there is no break in
+			// between the busy indication for loading the view's meta data
+			var oViewModel = new JSONModel({
+				busy: false,
+				delay: 0,
+				lineItemListTitle: this.getResourceBundle().getText("detailLineItemTableHeading"),
+				count: 0
+			});
 
-				this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
+			this.getRouter().getRoute("object").attachPatternMatched(this._onObjectMatched, this);
 
-				this.setModel(oViewModel, "detailView");
+			this.setModel(oViewModel, "detailView");
 
-				this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
-			},
+			this.getOwnerComponent().getModel().metadataLoaded().then(this._onMetadataLoaded.bind(this));
+		},
 
-			/* =========================================================== */
-			/* event handlers                                              */
-			/* =========================================================== */
+		/* =========================================================== */
+		/* event handlers                                              */
+		/* =========================================================== */
 
-			/**
-			 * Event handler when the share by E-Mail button has been clicked
-			 * @public
-			 */
-			onShareEmailPress : function () {
-				var oViewModel = this.getModel("detailView");
+		/**
+		 * After list data is available, this handler method updates the
+		 * icon tab filter counter 
+		 * @param {sap.ui.base.Event} oEvent the update finished event
+		 * @public
+		 */
+		onUpdateFinished: function(oEvent) {
+			// update the master list object counter after new data is loaded
+			this._updateListItemCount(oEvent.getParameter("total"));
 
-				sap.m.URLHelper.triggerEmail(
-					null,
-					oViewModel.getProperty("/shareSendEmailSubject"),
-					oViewModel.getProperty("/shareSendEmailMessage")
-				);
-			},
+		},
 
+		/**
+		 * Event handler when the share by E-Mail button has been clicked
+		 * @public
+		 */
+		onShareEmailPress: function() {
+			var oViewModel = this.getModel("detailView");
 
-			/**
-			 * Updates the item count within the line item table's header
-			 * @param {object} oEvent an event containing the total number of items in the list
-			 * @private
-			 */
-			onListUpdateFinished : function (oEvent) {
-				var sTitle,
-					iTotalItems = oEvent.getParameter("total"),
-					oViewModel = this.getModel("detailView");
+			sap.m.URLHelper.triggerEmail(
+				null,
+				oViewModel.getProperty("/shareSendEmailSubject"),
+				oViewModel.getProperty("/shareSendEmailMessage")
+			);
+		},
 
-				// only update the counter if the length is final
-				if (this.byId("lineItemsList").getBinding("items").isLengthFinal()) {
-					if (iTotalItems) {
-						sTitle = this.getResourceBundle().getText("detailLineItemTableHeadingCount", [iTotalItems]);
-					} else {
-						//Display 'Line Items' instead of 'Line items (0)'
-						sTitle = this.getResourceBundle().getText("detailLineItemTableHeading");
-					}
-					oViewModel.setProperty("/lineItemListTitle", sTitle);
+		/**
+		 * Updates the item count within the line item table's header
+		 * @param {object} oEvent an event containing the total number of items in the list
+		 * @private
+		 */
+		onListUpdateFinished: function(oEvent) {
+			var sTitle,
+				iTotalItems = oEvent.getParameter("total"),
+				oViewModel = this.getModel("detailView");
+
+			// only update the counter if the length is final
+			if (this.byId("lineItemsList").getBinding("items").isLengthFinal()) {
+				if (iTotalItems) {
+					sTitle = this.getResourceBundle().getText("detailLineItemTableHeadingCount", [iTotalItems]);
+				} else {
+					//Display 'Line Items' instead of 'Line items (0)'
+					sTitle = this.getResourceBundle().getText("detailLineItemTableHeading");
 				}
-			},
+				oViewModel.setProperty("/lineItemListTitle", sTitle);
+			}
+		},
 
-			/* =========================================================== */
-			/* begin: internal methods                                     */
-			/* =========================================================== */
+		/* =========================================================== */
+		/* begin: internal methods                                     */
+		/* =========================================================== */
 
-			/**
-			 * Binds the view to the object path and expands the aggregated line items.
-			 * @function
-			 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
-			 * @private
-			 */
-			_onObjectMatched : function (oEvent) {
-				var sObjectId =  oEvent.getParameter("arguments").objectId;
-				var sItemId =  oEvent.getParameter("arguments").itemId;
-				this.getModel().metadataLoaded().then( function() {
-					var sObjectPath = this.getModel().createKey("ocrInvoiceLSet", {
-						Document :  sObjectId,
-						Buzei	: sItemId
-					});
-					this._bindView("/" + sObjectPath);
-				}.bind(this));
-			},
+		/**
+		 * Binds the view to the object path and expands the aggregated line items.
+		 * @function
+		 * @param {sap.ui.base.Event} oEvent pattern match event in route 'object'
+		 * @private
+		 */
+		_onObjectMatched: function(oEvent) {
+			var sObjectId = oEvent.getParameter("arguments").objectId;
+			var sItemId = oEvent.getParameter("arguments").itemId;
+			this.getModel().metadataLoaded().then(function() {
+				var sObjectPath = this.getModel().createKey("ocrInvoiceSet", {
+					Document: sObjectId,
+					Buzei: sItemId
+				});
+				this._bindView("/" + sObjectPath);
+			}.bind(this));
+		},
 
-			/**
-			 * Binds the view to the object path. Makes sure that detail view displays
-			 * a busy indicator while data for the corresponding element binding is loaded.
-			 * @function
-			 * @param {string} sObjectPath path to the object to be bound to the view.
-			 * @private
-			 */
-			_bindView : function (sObjectPath) {
-				// Set busy indicator during view binding
-				var oViewModel = this.getModel("detailView");
+		/**
+		 * Sets the item count on the search icon tab filter
+		 * @param {integer} iTotalItems the total number of items in the list
+		 * @private
+		 */
+		_updateListItemCount: function(iTotalItems) {
+			this.getModel("detailView").setProperty("/count", iTotalItems);
+		},
 
-				// If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
-				oViewModel.setProperty("/busy", false);
+		/**
+		 * Binds the view to the object path. Makes sure that detail view displays
+		 * a busy indicator while data for the corresponding element binding is loaded.
+		 * @function
+		 * @param {string} sObjectPath path to the object to be bound to the view.
+		 * @private
+		 */
+		_bindView: function(sObjectPath) {
+			// Set busy indicator during view binding
+			var oViewModel = this.getModel("detailView");
 
-				this.getView().bindElement({
-					path : sObjectPath,
-					events: {
-						change : this._onBindingChange.bind(this),
-						dataRequested : function () {
-							oViewModel.setProperty("/busy", true);
-						},
-						dataReceived: function () {
-							oViewModel.setProperty("/busy", false);
-						}
+			// If the view was not bound yet its not busy, only if the binding requests data it is set to busy again
+			oViewModel.setProperty("/busy", false);
+
+			this.getView().bindElement({
+				path: sObjectPath,
+				events: {
+					change: this._onBindingChange.bind(this),
+					dataRequested: function() {
+						oViewModel.setProperty("/busy", true);
+					},
+					dataReceived: function() {
+						oViewModel.setProperty("/busy", false);
 					}
-				});
-			},
-
-			_onBindingChange : function () {
-				var oView = this.getView(),
-					oElementBinding = oView.getElementBinding();
-
-				// No data for the binding
-				if (!oElementBinding.getBoundContext()) {
-					this.getRouter().getTargets().display("detailObjectNotFound");
-					// if object could not be found, the selection in the master list
-					// does not make sense anymore.
-					this.getOwnerComponent().oListSelector.clearMasterListSelection();
-					return;
 				}
+			});
+		},
 
-				var sPath = oElementBinding.getPath(),
-					oResourceBundle = this.getResourceBundle(),
-					oObject = oView.getModel().getObject(sPath),
-					sObjectId = oObject.Document,
-					sObjectName = oObject.RdVendorName,
-					oViewModel = this.getModel("detailView");
+		_onBindingChange: function() {
+			var oView = this.getView(),
+				oElementBinding = oView.getElementBinding();
 
-				this.getOwnerComponent().oListSelector.selectAListItem(sPath);
-
-				oViewModel.setProperty("/shareSendEmailSubject",
-					oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
-				oViewModel.setProperty("/shareSendEmailMessage",
-					oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
-			},
-
-			_onMetadataLoaded : function () {
-				// Store original busy indicator delay for the detail view
-				var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
-					oViewModel = this.getModel("detailView"),
-					oLineItemTable = this.byId("lineItemsList"),
-					iOriginalLineItemTableBusyDelay = oLineItemTable.getBusyIndicatorDelay();
-
-				// Make sure busy indicator is displayed immediately when
-				// detail view is displayed for the first time
-				oViewModel.setProperty("/delay", 0);
-				oViewModel.setProperty("/lineItemTableDelay", 0);
-
-				oLineItemTable.attachEventOnce("updateFinished", function() {
-					// Restore original busy indicator delay for line item table
-					oViewModel.setProperty("/lineItemTableDelay", iOriginalLineItemTableBusyDelay);
-				});
-
-				// Binding the view will set it to not busy - so the view is always busy if it is not bound
-				oViewModel.setProperty("/busy", true);
-				// Restore original busy indicator delay for the detail view
-				oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
+			// No data for the binding
+			if (!oElementBinding.getBoundContext()) {
+				this.getRouter().getTargets().display("detailObjectNotFound");
+				// if object could not be found, the selection in the master list
+				// does not make sense anymore.
+				this.getOwnerComponent().oListSelector.clearMasterListSelection();
+				return;
 			}
 
-		});
+			var sPath = oElementBinding.getPath(),
+				oResourceBundle = this.getResourceBundle(),
+				oObject = oView.getModel().getObject(sPath),
+				sObjectId = oObject.Document,
+				sObjectName = oObject.RdVendorName,
+				oViewModel = this.getModel("detailView");
 
-	}
-);
+			this.getOwnerComponent().oListSelector.selectAListItem(sPath);
+
+			oViewModel.setProperty("/shareSendEmailSubject",
+				oResourceBundle.getText("shareSendEmailObjectSubject", [sObjectId]));
+			oViewModel.setProperty("/shareSendEmailMessage",
+				oResourceBundle.getText("shareSendEmailObjectMessage", [sObjectName, sObjectId, location.href]));
+		},
+
+		_onMetadataLoaded: function() {
+			// Store original busy indicator delay for the detail view
+			var iOriginalViewBusyDelay = this.getView().getBusyIndicatorDelay(),
+				oViewModel = this.getModel("detailView"),
+				oLineItemTable = this.byId("lineItemsList"),
+				iOriginalLineItemTableBusyDelay = oLineItemTable.getBusyIndicatorDelay();
+
+			// Make sure busy indicator is displayed immediately when
+			// detail view is displayed for the first time
+			oViewModel.setProperty("/delay", 0);
+			oViewModel.setProperty("/lineItemTableDelay", 0);
+
+			oLineItemTable.attachEventOnce("updateFinished", function() {
+				// Restore original busy indicator delay for line item table
+				oViewModel.setProperty("/lineItemTableDelay", iOriginalLineItemTableBusyDelay);
+			});
+
+			// Binding the view will set it to not busy - so the view is always busy if it is not bound
+			oViewModel.setProperty("/busy", true);
+			// Restore original busy indicator delay for the detail view
+			oViewModel.setProperty("/delay", iOriginalViewBusyDelay);
+		}
+
+	});
+
+});
